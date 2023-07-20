f380068c98ec5677d0b9055f4b41a37892a8f000
MP <mp@swoop.pro>
2023-07-20 11:33:08 +0300

    task24 done - calculate sum of numbers from one to input-minor improvements

---
+++ seminar/task24/Program.cs
@@ -0,0 +1,23 @@
+﻿// calculate sum of numbers from one to input
+// 4--> 10
+// 7-->28
+// 8-->36
+
+Console.WriteLine("please input number");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+if (number <= 0)
+{
+    Console.WriteLine("wrong input");
+}
+
+else
+
+for (int i = 1; i <= number; i++)
+{
+    // result = result + i;
+    result += i;
+}
+
+Console.WriteLine($"Sum is: {result}");
