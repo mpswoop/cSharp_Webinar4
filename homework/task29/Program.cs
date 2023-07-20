755cf217f4179d0eb0a1750c2e57fd14631c2e07
MP <mp@swoop.pro>
2023-07-20 20:22:51 +0300

    task 29 done - 8 elements array wirh  random generation of elements

---
+++ homework/task29/Program.cs
@@ -0,0 +1,10 @@
+﻿// Array of 8 elements using random numbers
+Console.WriteLine("this is an array of 8 random numbers");
+
+int[] numbers = new int[8];
+
+for (int i = 0; i < 8; i++)
+{
+    numbers[i] = new Random().Next(0, 1000000000);
+    Console.WriteLine($"Element #{i+1}: {numbers[i]}");
+}    
