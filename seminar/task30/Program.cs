f21a1ae08a98c6d2504dfc2c783ce16d4d399ac0
MP <mp@swoop.pro>
2023-07-20 16:15:46 +0300

    Task 30 done - create array of 8 random elements between 0 and 1

---
+++ seminar/task30/Program.cs
@@ -0,0 +1,10 @@
+﻿// create array of 8 random elements between 0 and 1
+Console.WriteLine("array of 8");
+
+int[] numbers = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    numbers[i] = new Random().Next(0, 10);
+    Console.Write(numbers[i]);
+}
+Console.WriteLine();
