3897469e15799b955b8679c684eb2249825a6d3f
MP <mp@swoop.pro>
2023-07-20 15:05:16 +0300

    task26 - digits counter doneD

---
+++ seminar/task26/Program.cs
@@ -0,0 +1,13 @@
+﻿// input number, output no of digits
+Console.WriteLine("input number");
+int num = Convert.ToInt32(Console.ReadLine());
+int digits = 0;
+
+for (int i = 0; num > 0; i++)
+
+{
+    num = num / 10;
+    digits++ ;
+}
+        
+Console.WriteLine($"the number has {digits} digits");
